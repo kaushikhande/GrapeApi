e24b3e245c77d25b2f5a24fc9bb785260cf1a034
kaushikhande <kaushikhande302@gmail.com>
2018-11-24 01:01:03 +0530

    part-1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  config.paths.add File.join(‘app’, ‘api’), glob: File.join(‘**’, ‘*.rb’)
+  config.autoload_paths += Dir[Rails.root.join(‘app’, ‘api’, ‘*’)]
 end
